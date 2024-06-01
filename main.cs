f8e6908b2993ddfa1861aecff0ac5f0cb84105e6
emathis6 <156727774+emathis6@users.noreply.github.com>
2024-06-01 22:53:56 +0000

    Module 2: Exercise 2.1 (InchesToCentimeters)

--- main.cs
+++ main.cs
@@ -1,30 +1,22 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine("Oh, well imagine /");
-    Console.WriteLine("As I'm pacing the pews in a church corridor /");
-    Console.WriteLine("And I can't help but to hear /");
-    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
-    Console.WriteLine();
-
-    Console.WriteLine("Oh-oh-oh-oh-oh,");
-    Console.WriteLine("oh-oh-oh-oh,");
-    Console.WriteLine("oh-oh-oh /");
-    Console.WriteLine("Caught in a bad romance...");
-    Console.WriteLine();
-
-    Console.WriteLine("Coming out of my cage");
-    Console.WriteLine("And I've been doing just fine");
-    Console.WriteLine("Gotta gotta be down");
-    Console.WriteLine("Because I want it all");
-    Console.WriteLine();
-
-    Console.WriteLine("Now, this is a story all about how");
-    Console.WriteLine("My life got flipped-turned upside down");
-    Console.WriteLine("And I'd like to take a minute");
-    Console.WriteLine("Just sit right there");
-    Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
-    Console.WriteLine();
+ public static void Main (string[] args) {
+   const double Centimeters = 2.54;
+   int inches = 3;
+   int test1 = 5;
+   int test2 = 50;
+   int test3 = -15;
+   int test4 = 15994;
+   double centimeters = inches * Centimeters;
+   double centimeters1 = test1 * Centimeters;
+   double centimeters2 = test2 * Centimeters;
+   double centimeters3 = test3 * Centimeters;
+   double centimeters4 = test4 * Centimeters;
+   Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters); 
+   Console.WriteLine("{0} inches is {1} centimeters", test1, centimeters1); 
+   Console.WriteLine("{0} inches is {1} centimeters", test2, centimeters2); 
+   Console.WriteLine("{0} inches is {1} centimeters", test3, centimeters3); 
+   Console.WriteLine("{0} inches is {1} centimeters", test4, centimeters4); 
   }
-}
+}  
