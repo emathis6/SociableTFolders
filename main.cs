f16448db23b293d444223882f30803f7d69b9789
emathis6 <156727774+emathis6@users.noreply.github.com>
2024-06-02 15:54:11 +0000

    Module 2: Exercise 2.1.1 (InchesToCentimetersInteractive)

--- main.cs
+++ main.cs
@@ -8,23 +8,12 @@ class Program {
    
    if (double.TryParse(input, out double inches)){
      double centimeters = inches * Centimeters;
-     Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);     
+     Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);  
    }
-  
-     
-   
-   //Console.WriteLine("Please enter another number of inches: ");
-   //var x = Console.ReadLine();
+   else {
+     Console.WriteLine("Invalid input");
+   }
 
+ }    
 
-   //double centimeters1 = test1 * Centimeters;
-   //double centimeters2 = test2 * Centimeters;
-   //double centimeters3 = test3 * Centimeters;
-   //double centimeters4 = test4 * Centimeters;
-
-   //Console.WriteLine("{0} inches is {1} centimeters", test1, centimeters1);
-   //Console.WriteLine("{0} inches is {1} centimeters", test2, centimeters2); 
-   //Console.WriteLine("{0} inches is {1} centimeters", test3, centimeters3); 
-   //Console.WriteLine("{0} inches is {1} centimeters", test4, centimeters4); 
-  }
 }
