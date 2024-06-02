21abc36b37db2955bfe8b00c058fecbc3ca80016
emathis6 <156727774+emathis6@users.noreply.github.com>
2024-06-02 15:38:27 +0000

    Module 2: Exercise 2.1.1 (InchesToCentimetersInteractive)

--- main.cs
+++ main.cs
@@ -3,20 +3,28 @@ using System;
 class Program {
  public static void Main (string[] args) {
    const double Centimeters = 2.54;
-   int inches = 3;
-   int test1 = 5;
-   int test2 = 50;
-   int test3 = -15;
-   int test4 = 15994;
-   double centimeters = inches * Centimeters;
-   double centimeters1 = test1 * Centimeters;
-   double centimeters2 = test2 * Centimeters;
-   double centimeters3 = test3 * Centimeters;
-   double centimeters4 = test4 * Centimeters;
-   Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters); 
-   Console.WriteLine("{0} inches is {1} centimeters", test1, centimeters1); 
-   Console.WriteLine("{0} inches is {1} centimeters", test2, centimeters2); 
-   Console.WriteLine("{0} inches is {1} centimeters", test3, centimeters3); 
-   Console.WriteLine("{0} inches is {1} centimeters", test4, centimeters4); 
+   Console.WriteLine("Please enter measurements in inches: ");
+   var input = Console.ReadLine();
+   
+   if (double.TryParse(input, out double inches)){
+     double centimeters = inches * Centimeters;
+     Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);     
+   }
+  
+     
+   
+   //Console.WriteLine("Please enter another number of inches: ");
+   //var x = Console.ReadLine();
+
+
+   //double centimeters1 = test1 * Centimeters;
+   //double centimeters2 = test2 * Centimeters;
+   //double centimeters3 = test3 * Centimeters;
+   //double centimeters4 = test4 * Centimeters;
+
+   //Console.WriteLine("{0} inches is {1} centimeters", test1, centimeters1);
+   //Console.WriteLine("{0} inches is {1} centimeters", test2, centimeters2); 
+   //Console.WriteLine("{0} inches is {1} centimeters", test3, centimeters3); 
+   //Console.WriteLine("{0} inches is {1} centimeters", test4, centimeters4); 
   }
-}  
+}
