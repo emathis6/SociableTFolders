64c77f29578d63a4d850999bdb6e045527658195
emathis6 <156727774+emathis6@users.noreply.github.com>
2024-06-10 00:42:21 +0000

    Module 3 Exercise 3.2 (Hurricane Category)

--- main.cs
+++ main.cs
@@ -1,23 +1,47 @@
 using System;
+using System.Text.RegularExpressions;
 
 class Program {
  static void Main (string[] args) {
-   
-    const int MaxLength = 140;
-    Console.WriteLine("Enter your message:");
-    string message = Console.ReadLine();
-    if(message.Length > MaxLength)
+
+    Console.WriteLine("Enter wind speed in miles per hour:");
+    string input = Console.ReadLine();
+
+    string numericPart = Regex.Replace(input, @"[^0-9.]", "");
+
+    if (double.TryParse(numericPart, out double windSpeed))
+         
     {
-      Console.WriteLine("Rejected");
-
+      if (windSpeed >= 157){
+        Console.WriteLine("Category Five Hurricane");
+       } 
+       else if (windSpeed >= 130){
+        Console.WriteLine("Category Four Hurricane");
+       } 
+       else if (windSpeed >= 111){
+        Console.WriteLine("Category Three Hurricane");
        }
+       else if (windSpeed >= 96){
+        Console.WriteLine("Category Two Hurricane");
+       }
+       else if (windSpeed >= 74){
+        Console.WriteLine("Category One Hurricane");
+       }
+       else   
+       {
+        Console.WriteLine("Not a Hurricane");
+       }
+    }
     else
-      Console.WriteLine("Posted");
+    {
+       Console.WriteLine("Invalid input. Please enter a valid number.");
+    }
+  }
+}
+
+
+
     
-   }
-
- }    
-
 
   
    
